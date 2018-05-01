cbb4de740d856fbfd7c20ae39b7dabc7e74a334a
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-05-01 10:25:51 +0800

    Added common for printing data

---
+++ controllers/common.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+// OutputDataAsJSON outputs data as JSON format
+func OutputDataAsJSON(c *gin.Context, data gin.H, status, msg string) {
+	c.JSON(200, gin.H{
+		"status":  status,
+		"message": msg,
+		"data":    data,
+	})
+}
+
+// OutputJSON outputs message as JSON format
+func OutputJSON(c *gin.Context, status, msg string) {
+	c.JSON(200, gin.H{
+		"status":  status,
+		"message": msg,
+	})
+}
+
+// OutputError outputs error message as JSON format
+func OutputError(c *gin.Context) {
+	c.JSON(500, gin.H{
+		"status":  "error",
+		"message": errors.New("something went wrong"),
+	})
+}
