92fe62fa61ae91d5448115fd485c280a91f5c5e0
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-07-23 22:03:43 +0800

    Added graceful recovery when error
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/BottleneckStudio/WordJar/controllers"
+	"github.com/BottleneckStudio/WordJar/middlewares/cors"
 	"github.com/BottleneckStudio/WordJar/middlewares/gzip"
 	"github.com/gin-gonic/gin"
 )
@@ -11,11 +12,16 @@ import (
 var port = ":" + os.Getenv("PORT")
 
 func main() {
-
 	router := gin.Default()
+
+	// Add CORS middleware
+	router.Use(cors.Middleware())
 	// Before initializing routes,
 	// all response should be gzipped
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	// Recover from panic/errors
+	router.Use(gin.Recovery())
 	initializeRoutes(router)
 
 	router.Run(port)
