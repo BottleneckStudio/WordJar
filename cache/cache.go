768f9da764d792e36cd62fedec52ea588e6003fc
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-08-18 22:58:45 +0800

    Added interface for caching service
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- cache/cache.go
+++ cache/cache.go
@@ -1,147 +1,24 @@
 package cache
 
-import (
-	"bufio"
-	"bytes"
-	"compress/gzip"
-	"os"
-
-	"github.com/bradfitz/gomemcache/memcache"
-)
-
-var mc *memcache.Client
-
-const (
-	compress = true
-)
-
-var prefix = "mycache."
-
-var memcachedHostStr string
-
-func init() {
-	mhost := getenvWithDefault("MEMCACHED_HOST", "localhost")
-	mport := getenvWithDefault("MEMCACHED_PORT", "11211")
-	memcachedHostStr = mhost + ":" + mport
-	mc = memcache.New(memcachedHostStr)
-
+// Cacher defines the different
+// methods of caching
+type Cacher interface {
+	Set(string, interface{}) (bool, error)
+	Get(string) (interface{}, error)
+	Delete(string) (bool, error)
 }
 
-// GetHostStr ...
-func GetHostStr() string {
-	return memcachedHostStr
+// Set ...
+func Set(key string, data interface{}, cacher Cacher) (bool, error) {
+	return cacher.Set(key, data)
 }
 
-// Get gets the value inside memcache.
-func Get(suffix string) (string, error) {
-	var key string
-	if compress {
-		key = prefix + ".c." + suffix
-	} else {
-		key = prefix + suffix
-	}
-
-	it, err := mc.Get(key)
-	if err != nil {
-		return "", err
-	}
-	if compress {
-		return gzuncompress(it.Value)
-	}
-	return string(it.Value), nil
-}
-
-// Set sets value to memcache.
-func Set(suffix string, val string) (bool, error) {
-	var key string
-	var e error
-	if compress {
-		key = prefix + ".c." + suffix
-		e = mc.Set(&memcache.Item{
-			Key:        key,
-			Value:      gzcompress(val),
-			Expiration: 0,
-		})
-	} else {
-		key = prefix + suffix
-		e = mc.Set(&memcache.Item{
-			Key:        key,
-			Value:      []byte(val),
-			Expiration: 0,
-		})
-	}
-
-	if e != nil {
-		return false, e
-	}
-	return true, nil
+// Get ...
+func Get(key string, cacher Cacher) (interface{}, error) {
+	return cacher.Get(key)
 }
 
 // Delete ...
-func Delete(suffix string) (bool, error) {
-	key := prefix + suffix
-	if compress {
-		key = prefix + ".c." + suffix
-	}
-
-	e := mc.Delete(key)
-
-	if e != nil {
-		return false, e
-	}
-
-	return true, nil
-}
-
-// getEnvWithDefault returns the default key
-// if no environment variable has been set.
-func getenvWithDefault(key string, def string) string {
-	v := os.Getenv(key)
-	if v == "" {
-		return def
-	}
-	return v
-}
-
-func gzcompress(str string) []byte {
-	var b bytes.Buffer
-
-	gz := gzip.NewWriter(&b)
-
-	if _, err := gz.Write([]byte(str)); err != nil {
-		return []byte("")
-	}
-	if err := gz.Flush(); err != nil {
-		return []byte("")
-	}
-	if err := gz.Close(); err != nil {
-		return []byte("")
-	}
-	return b.Bytes()
-
-}
-
-func gzuncompress(b []byte) (string, error) {
-	bb := bytes.NewBuffer(b)
-	zipread, _ := gzip.NewReader(bb)
-
-	defer zipread.Close()
-	reader := bufio.NewReader(zipread)
-
-	var (
-		part []byte
-		err  error
-	)
-	ret := ""
-
-	for {
-		if part, _, err = reader.ReadLine(); err != nil {
-			break
-		}
-
-		ret += string(part)
-
-	}
-	return ret, nil
-
+func Delete(key string, cacher Cacher) (bool, error) {
+	return cacher.Delete(key)
 }
